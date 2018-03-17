0b5d25514d2854a0637b781e62580bdab09415e2
kt66nf <30427798+youngwook-kim@users.noreply.github.com>
2018-03-17 06:21:25 -0700

    Got rid of ensemble part.

--- Wisconsin Breast Cancer Data.py
+++ Wisconsin Breast Cancer Data.py
@@ -2,8 +2,8 @@
 """
 Created on Mon Feb 12 20:46:05 2018
 
-@author: Youngwook
 """
+
 from __future__ import print_function
 
 from hyperopt import Trials, STATUS_OK, tpe, rand
@@ -14,6 +14,7 @@ from keras.utils import np_utils
 from sklearn.metrics import accuracy_score
 from hyperas import optim
 from hyperas.distributions import choice, uniform, conditional
+from keras import optimizers
 
 
 def data():
@@ -55,15 +56,21 @@ def create_model(X_train, y_train, X_val, y_val):
     model = models.Sequential()
     model.add(
         layers.Dense({{choice([np.power(2, 5), np.power(2, 6), np.power(2, 7)])}}, input_shape=(len(data.columns),)))
-    model.add(LeakyReLU(alpha={{uniform(0, 1)}}))
-    model.add(Dropout({{uniform(0, 1)}}))
+    model.add(LeakyReLU(alpha={{uniform(0.5, 1)}}))
+    model.add(Dropout({{uniform(0.5, 1)}}))
     model.add(layers.Dense({{choice([np.power(2, 3), np.power(2, 4), np.power(2, 5)])}}))
     # model.add(Activation({{choice([ 'tanh'])}}))
-    model.add(LeakyReLU(alpha={{uniform(0, 1)}}))
-    model.add(Dropout({{uniform(0, 1)}}))
+    model.add(LeakyReLU(alpha={{uniform(0.5, 1)}}))
+    model.add(Dropout({{uniform(0.5, 1)}}))
     model.add(layers.Dense(1, activation='sigmoid'))
 
-    from keras import optimizers
+    from keras import callbacks
+
+    keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=0, mode='auto',
+                                      epsilon=0.0001, cooldown=0, min_lr=0)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                                  patience=5, min_lr=0.001)
+    model.fit(X_train, Y_train, callbacks=[reduce_lr])
 
     model.compile(optimizer={{choice(['rmsprop', 'adam', 'sgd'])}},
                   loss='binary_crossentropy',
@@ -84,24 +91,12 @@ if __name__ == '__main__':
     best_run, best_model = optim.minimize(model=create_model,
                                           data=data,
                                           algo=tpe.suggest,
-                                          max_evals=5,
+                                          max_evals=15,
                                           trials=Trials())
     X_train, X_val, X_test, y_train, y_val, y_test = data()
     print("Evalutation of best performing model:")
     print(best_model.evaluate(X_test, y_test))
     print("Best performing model chosen hyper-parameters:")
     print(best_run)
-    '''
-    X_train, X_val, X_test, y_train, y_val, y_test = data()
-    ensemble_model = optim.best_ensemble(nb_ensemble_models=5,
-                                         model=create_model,
-                                         data=data,
-                                         algo=rand.suggest,
-                                         max_evals=10,
-                                         trials=Trials(),
-                                         voting='hard')
-    preds = ensemble_model.predict(X_test)
-    y_test = np_utils.categorical_probas_to_classes(y_test)
-    print(accuracy_score(preds, y_test))
 
-    '''
+    best_model.save('breast_cancer_model.h5')
